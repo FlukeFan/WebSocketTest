ffd045cc66c481f82db98d4e8235e33a9f644df9
Richard Brown <flukefan@googlemail.com>
2013-12-09 11:31:11 +0000

    Initial commit.

---
+++ src/CommandPrompt.bat
@@ -0,0 +1,7 @@
+@CD /D "%~dp0"
+@title WebSocketTest Command Prompt
+@SET PATH=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\;%PATH%
+@doskey build=msbuild WebSocketTest.proj $*
+@doskey run=msbuild WebSocketTest.proj /t:Run $*
+type readme.txt
+%comspec%
